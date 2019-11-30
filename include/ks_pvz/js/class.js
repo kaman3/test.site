865a8e61aff107072e869c60858d563c5def13a1
Sergey <aligoweb@ya.ru>
2019-11-30 11:41:40 +0300

    add function createBallon

--- include/ks_pvz/js/class.js
+++ include/ks_pvz/js/class.js
@@ -39,8 +39,67 @@
 	}
 	// собираем ballon для точек на карте
 	createTemplateBallon(idObject, dataGeoPoint, maxPeriodDelivery){
-         console.log(dataGeoPoint);
-	}
+		                      
+		let time = JSON.parse(dataGeoPoint.issue); // часы работы пункта выдачи
+		let line = '';
+ 
+		for (let key in time) {
+			 line += '<div style = "display:inline-block">'+
+						  '<div class = "timeExtradition">'+
+								'<div style = "font-weight:bold;">'+key+'</div>'+
+								'<div style = "">'+time[key].startIssue+'</div>'+
+								'<div style = "">'+time[key].endIssue+'</div>'+
+						  '</div>'+
+					 '</div>';
+		}
+		
+		let dTime = '';
+		if(maxPeriodDelivery !== undefined && maxPeriodDelivery != ''){
+		    dTime = maxPeriodDelivery.replace("'","").replace("'","");
+		}else{
+		    dTime = 'уточните у оператора интернет-магазина';
+		}
+		
+		let dPhone = '';
+		if(dataGeoPoint.phone !== undefined && dataGeoPoint.phone != ''){
+		   dPhone = dataGeoPoint.phone;
+		}else{
+		   dPhone = 'на точке нет телефона';
+		}
+ 
+		const termsReceipt = '<div class = "termsReceipt">'+
+							   '<p>Внимание! Для получения товара необходимо при себе иметь паспорт.</p>'+
+							   '<button id = "btnTR">Понятно</button>'+
+						   '</div>';
+		
+ 
+		const balloonHeader = '<div class = "bHeader" id = "'+idObject+'" style = "padding:5px 0 5px 0; font-weight:bold; color:#000; line-height:17px;">'+dataGeoPoint.name+'</div>';
+ 
+		const ballonHtml = '<div style = "font-size:14px; font-weight:normal; position:relative">'+
+				'<div style = "padding:0 0 5px 0"><span style = "color:#767676">Адрес: </span>'+dataGeoPoint.adress+'</div>'+
+				'<div style = "padding:0 0 5px 0"><span style = "color:#767676">Телефон: </span>'+dPhone+'</div>'+
+				'<div style = "padding:0 0 5px 0"><span style = "color:#767676">Срок доставки в днях:</span> '+dTime+' </div>'+
+				'<div id = "paymentCard" style = "padding:0 0 5px 0;"><span style = "color:#767676">Оплата банковской картой:</span> '+dataGeoPoint.paymentByCard+' </div>'+
+				'<div id = "paymentCash" style = "padding:0 0 5px 0"><span style = "color:#767676">Оплата наличными:</span> '+dataGeoPoint.paymentByCash+' </div>'+       
+				'<div class = "boxTermReceipt"><p>Обязательно ознакомьтесь с условиями получения товара</p>'+termsReceipt+'</div>'+
+				'<div style = "padding:0 0 5px 0"><span style = "color:#767676">Срок хранения в днях:</span> '+dataGeoPoint.storageLife+' </div>'+
+				'<div style = "padding:5px 0 5px 0; font-weight:bold">Часы выдачи товара</div>'+
+				'<div class = "timeExtradition">'+line+'</div>'+
+				'<div class = "buttonsBallon">'+
+				'<a class = "btnSelectBallon selectGuid">Выбрать</a>'+
+				'<a class = "btnCloseBallon">Отмена</a>'+
+				'</div>'+
+		 '</div>';
+		 
+		 let PointHtml = {
+			 id:idObject,
+			 header:balloonHeader,
+			 body:ballonHtml,
+		 };
+ 
+	   return PointHtml;
+ 
+	 }
 	// создаем меню
 	createMapMenu(dataGeoPoint){
 
@@ -49,7 +108,8 @@
 		if(typeof dataGeoPoint !== undefined && Object.keys(dataGeoPoint.pointPvz).length > 0){
 
 			dataGeoPoint.pointPvz.forEach((element) => {
-				submenuItem += '<li class = "point" id = "'+ element.id +'" data_id_cse = "'+element.guid+'">'+ element.adress + '</li>';
+				let setTemplateBallon = this.createTemplateBallon(element.id, element, dataGeoPoint.deliveryTime.maxPeriod);
+				submenuItem += '<li class = "point" id = "'+ element.id +'" data_id_cse = "'+element.guid+'">'+ element.adress + '</li><div class = "dpoint" id = "'+ element.id +'">'+setTemplateBallon.body+'</div>';
 			});
 		
 			return (typeof submenuItem == 'string' && submenuItem.length > 0) ? submenuItem : submenuItem = 'В вашем городе нет точек самовывоза';
